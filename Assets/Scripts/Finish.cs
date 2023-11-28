6d3414e5d679d6e4b8db7c45c83eb5e72db15193
tep98 <Korotkovstepan95@gmail.com>
2023-11-28 03:23:01 +0300

    feat: make localization on main menu

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using YG;
 
 public class Finish : MonoBehaviour
 {
     [SerializeField] private GameObject finishUI;
     [SerializeField] private GameObject deathCollider;
 
+    private void Start()
+    {
+        YandexGame.FullscreenShow();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
